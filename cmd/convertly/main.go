de1ff66975e8c88c4ed6767ce5322a1f2d8e4495
Will Madison <wmadisondev@gmail.com>
2018-08-23 00:05:09 -0400

    adding server startup code

--- cmd/convertly/main.go
+++ cmd/convertly/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/willmadison/convertly"
+)
+
 func main() {
 
+	http.Handle("/", convertly.RootHandler(convertly.NewFixerExchanger("43d8eaede49bad82ef3e2c4dbcfafbbe")))
+
+	fmt.Println("Convertly serving requests on localhost:8888")
+	http.ListenAndServe(":8888", nil)
 }
